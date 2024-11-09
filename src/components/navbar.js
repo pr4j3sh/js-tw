ab1db8eb94f7756bfd6a63b25bbd33a17c04454a
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-11-10 02:48:30 +0530

    fixed btn text by aria label

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ const component = `<nav>
 <a href="/" class="font-semibold">${SITE.REPO}</a>
         <ul>
           <li>
-            <button class="icon" id="theme-toggle">Theme</button>
+            <button class="icon" id="theme-toggle" aria-label="Theme"></button>
           </li>
         </ul>
       </nav>`;
